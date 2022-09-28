f338aba5de11d48922496004a159ad95b292dd63
Vichiuri <chiuri.vic@gmail.com>
2022-09-28 07:40:23 +0300

    controllers

--- index.ts
+++ index.ts
@@ -1,9 +1,12 @@
 
 import express, { Application, Request, Response } from 'express'
+import routes from './api/routes'
 
 
 const app: Application = express()
 const port = 3000
+
+app.use('/api/v1', routes)
 
 // Body parsing Middleware
 app.use(express.json());
